b959bd43f362161a328fe977668991cb7b732054
Phuc8192 <nguyenphucc10@gmail.com>
2022-01-20 02:08:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var images = ["images/Banner/1.jpg","images/Banner/2.jpg", "images/Banner/3.jpg"
 "images/Banner/11.jpg", "images/Banner/12.jpg", "images/Banner/13.jpg"];
 var banner, i=0;
 var loop = setInterval(Slide,2200);
-function Slide()
+function Slide(i)
 {
     banner = document.getElementById("banner");
     banner.src = images[i];
