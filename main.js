58692ef093bd2e8d812ce6d7312a4a1c424c6b93
Phuc8192 <nguyenphucc10@gmail.com>
2022-01-20 02:29:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 var images = ["images/Banner/1.jpg","images/Banner/2.jpg", "images/Banner/3.jpg","images/Banner/4.jpg",
 "images/Banner/5.jpg","images/Banner/6.jpg", "images/Banner/7.jpg", "images/Banner/8.jpg", 
 "images/Banner/11.jpg", "images/Banner/12.jpg", "images/Banner/13.jpg"];
-var banner, i=0;
+var banner, i=0, index=1;
+var next, prev;
 var loop = setInterval(Slide,2200);
 function Slide(i)
 {
     banner = document.getElementById("banner");
+    next = document.getElementsByClassName("next");
+    prev = document.getElementsByClassName("prev");
     banner.src = images[i];
     i++;
+    if (next.onClick())
+    {
+        i++;
+        banner.src = images[i];
+    }
+    if (prev.onClick())
+    {
+        i--;
+        banner.src = images[i];
+    }
     if(i==images.length)
         i=0;
 }
+function next()
 function Over()
 {
     clearInterval(loop);
